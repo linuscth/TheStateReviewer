aebae8b7af6507b89675a8196e0c5a60f835b8bd
SixFourDev <sb_94@yahoo.com>
2023-07-13 21:20:01 -0700

    added userRoutes.js and userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,49 @@
+const { User } = require('../models');
+
+// User registration controller
+const createUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const newUser = await User.create({ username, email, password });
+
+        req.session.save(() => {
+            req.session.userId = newUser.id;
+            res.status(200).json(newUser);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+// User login controller
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email } });
+
+        if (!user || !user.checkPassword(password)) {
+            res.status(401).json({ message: 'Invalid email or password' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.userId = user.id;
+            res.status(200).json(user);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+// User logout controller
+const logoutUser = (req, res) => {
+    try {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+module.exports = { createUser, loginUser, logoutUser };
