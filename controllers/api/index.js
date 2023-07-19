ea5788e1a52aa4088439326fdf72f89c364a9998
Ting hon Linus cheung <linuscheung@Tings-MBP-2.home>
2023-07-18 18:34:51 -0700

    added dashboard handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,8 @@ const commentRoutes = require('./commentRoutes');
 const stateRoutes = require('./stateRoutes');
 
 
-router.use('/reviews', reviewRoutes);
+// router.use('/reviews', reviewRoutes);
 router.use('/comments', commentRoutes);
-router.use('/states', stateRoutes);
+// router.use('/states', stateRoutes);
 
 module.exports = router;
--- controllers/homepageRoute.js
+++ controllers/homepageRoute.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
-const withAuth = reuqire()
-router
+const withAuth = require('../utils/auth');
+const { Comment, Review, State, User } = require('../models')
+
+router.get('/dashboard', async (req, res) => {
+    try {
+        const reviewData = await Review.findAll({ include: { model: User, attributes: ['username'] } });
+        const reviews = reviewData.map((review) => review.get({ plain: true }))
+        console.log(reviews[0]);
+        res.render('dashboard', {
+            reviews,
+            logged_in: req.session.logged_in
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+module.exports = router; 
