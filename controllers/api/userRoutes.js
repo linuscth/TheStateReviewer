0edc47fff4d7879ff3c4ed9b410b9a9737c5dd71
SixFourDev <sb_94@yahoo.com>
2023-07-18 19:13:37 -0700

    fixed user login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { createUser, loginUser, logoutUser } = require('./userController');
 
-
 // User registration route
 router.post('/register', createUser);
 
